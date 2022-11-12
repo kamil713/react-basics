85bbe1fc494474ceef3b9201822b3ebdc160fbf4
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-12 22:54:12 +0100

    pass props to card component

--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,18 +1,17 @@
-import image from "../assets/images/katie-zaferes.png";
 import star from "../assets/images/star.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img src={image} className="card--image" />
+      <img src={`../assets/images/${props.img}`} className="card--image" />
       <div className="card--stats">
         <img src={star} className="card--star" />
-        <span>5.0</span>
-        <span className="gray">(6) • </span>
-        <span className="gray">USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
-      <p><span className="bold">From $136</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
   );
 }
