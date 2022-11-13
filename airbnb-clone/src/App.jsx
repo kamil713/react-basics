6afb49689c914508fd0501b1ee8fe515c78f7386
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-13 22:36:20 +0100

    airbnb-clone

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from './components/Card'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import cardsData from "./data";
+import "./App.css";
 
 function App() {
+  const cardElements = cardsData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        {...card}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card 
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={13}
-      />
+      <section className="cards-list">{cardElements}</section>
     </div>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
