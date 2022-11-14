5483708d9d8d8e96be6e3b1df6974bbbf5feb1a6
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-14 14:36:40 +0100

    add Meme component

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Header from './components/Header'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Meme from "./components/Meme";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
