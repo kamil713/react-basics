8b6ab2c2f7a4d694892d108af6e42e803aaef11e
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-15 17:21:52 +0100

    add state to Meme component

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,23 +1,35 @@
+import React from 'react'
+import memesData from "../memesData";
+
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url
+    }))
+  }
+
   return (
     <main>
       <div className="form">
-        <input 
-            type="text" 
-            placeholder="Top text" 
-            className="form--input" 
-        />
-        <input 
-            type="text" 
-            placeholder="Bottom text" 
-            className="form--input" 
-        />
-        <button 
-            className="form--button"
-        >
-                Get a new meme image 🖼
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
         </button>
       </div>
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }
