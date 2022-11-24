caeff25e38d848cdc2a9f9d48711f13b6bb83841
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-24 11:21:38 +0100

    Map array to Die components

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,33 +1,26 @@
-import ReactDOM from 'react-dom/client'
-import Die from './components/Die';
-import './App.css'
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Die from "./components/Die";
+import "./App.css";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6))
+      newDice.push(Math.ceil(Math.random() * 6));
     }
-    return newDice
+    return newDice;
   }
-  console.log(allNewDice());
+
+  const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
-      <div className='dice-container'>
-        <Die value={4} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={4} />
-        <Die value={3} />
-        <Die value={1} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
-  )
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
