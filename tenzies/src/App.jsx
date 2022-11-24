7ffa205f34f9c3258f5d15fc1a076fdc8117526f
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-24 11:40:50 +0100

    add Roll dice button

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -13,12 +13,17 @@ function App() {
     }
     return newDice;
   }
+  
+  function rollDice() {
+    setDice(allNewDice())
+  }
 
   const diceElements = dice.map((die) => <Die value={die} />);
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>Roll</button>
     </main>
   );
 }
