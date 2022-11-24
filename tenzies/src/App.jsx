2913fbcfdc106001229ad99988005596c7f3a2fd
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-24 10:51:45 +0100

    add Die component

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,9 +1,22 @@
 import ReactDOM from 'react-dom/client'
+import Die from './components/Die';
 import './App.css'
 
 function App() {
   return (
     <main>
+      <div className='dice-container'>
+        <Die value={4} />
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={4} />
+        <Die value={3} />
+        <Die value={1} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={1} />
+        <Die value={2} />
+      </div>
     </main>
   )
 }
