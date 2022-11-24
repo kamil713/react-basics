e422250d66fcfd5a70d051e9566cd1c0457f2e5a
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-11-24 10:23:16 +0100

    add tenzies setup

---
+++ tenzies/src/App.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
