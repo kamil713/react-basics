1816e1dae67dd39139104578cbfc1a1fa39409fe
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-05 17:12:28 +0100

    add logic for die assets

--- tenzies/src/components/Die.jsx
+++ tenzies/src/components/Die.jsx
@@ -1,10 +1,32 @@
 export default function Die(props) {
+  let dieNum;
+  switch (props.value) {
+    case 1:
+      dieNum = 'One'
+      break;
+    case 2:
+      dieNum = 'Two'
+      break;
+    case 3:
+      dieNum = 'Three'
+      break;
+    case 4:
+      dieNum = 'Four'
+      break;
+    case 5:
+      dieNum = 'Five'
+      break;
+    case 6:
+      dieNum = 'Six'
+      break;
+  }
+
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white",
+    backgroundImage: `url(./src/assets/diceSvg/die${dieNum}.svg)`
   };
+
   return (
-    <div className="die-face" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
-    </div>
+    <div className="die-face" style={styles} onClick={props.holdDice}></div>
   );
 }
