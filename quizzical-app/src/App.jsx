5cd63235e8464f952e4f860aea8253c8cab75742
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-06 15:28:12 +0100

    add first screen structure

--- quizzical-app/src/App.jsx
+++ quizzical-app/src/App.jsx
@@ -3,9 +3,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <main>
-      <h1>Quizzical</h1>
-      <p>Some description if needed</p>
-      <Button text="Start" />
+      <h1 className="title">Quizzical</h1>
+      <p className="description">Some description if needed</p>
+      <Button text="Start quiz" />
     </main>
   );
 }
