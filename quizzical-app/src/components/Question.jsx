3ffdf1740dfe79c64f2594edaa54e99f8d873672
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-07 16:15:45 +0100

    fix problem with innerHtml

--- quizzical-app/src/components/Question.jsx
+++ quizzical-app/src/components/Question.jsx
@@ -1,11 +1,16 @@
-import Answer from "./Answer"
+import Answer from "./Answer";
 
 export default function Question(props) {
-    return (
-        <div className="question-container">
-            <h2 className="question-text">{props.question}</h2>
-            <Answer />
-            <div className='line'></div>
-        </div>
-    )
+  return (
+    <div className="question-container">
+      {/* https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml */}
+      {/* We can use DOMPurify but it's app only for learning purpose */}
+      <h3
+        className="question-text"
+        dangerouslySetInnerHTML={{ __html: props.question }}
+      ></h3>
+      <Answer />
+      <div className="line"></div>
+    </div>
+  );
 }
