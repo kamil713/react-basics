05082f09989337b613f26a6c4619284ab991713a
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-11 23:42:43 +0100

    add logic for clicked concrete answer

--- quizzical-app/src/components/Question.jsx
+++ quizzical-app/src/components/Question.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
 import Answer from "./Answer";
+import { nanoid } from "nanoid";
 
 export default function Question(props) {
   const answerElements = props.answers.map((answer) => {
-    return <Answer answer={answer} />;
+    return (
+      <Answer
+        id={nanoid()}
+        answer={answer}
+        clicked={false}
+        toggleCorrect={toggleCorrect}
+      />
+    );
   });
+
+  const [answers, setAnswers] = useState(answerElements);
+  console.log(answers);
+
+  function toggleCorrect(id) {
+    setAnswers((oldAnswers) =>
+      oldAnswers.map((answer) => {
+        if (answer.props.id === id) {
+          return (
+            <Answer
+              id={answer.props.id}
+              answer={answer.props.answer}
+              clicked={true}
+              toggleCorrect={toggleCorrect}
+            />
+          );
+        }
+
+        return (
+          <Answer
+            id={answer.props.id}
+            answer={answer.props.answer}
+            clicked={false}
+            toggleCorrect={toggleCorrect}
+          />
+        );
+      })
+    );
+  }
 
   return (
     <div className="question-container">
@@ -13,7 +51,7 @@ export default function Question(props) {
         className="question-text"
         dangerouslySetInnerHTML={{ __html: props.question }}
       ></h3>
-      <div className="answer-container">{answerElements}</div>
+      <div className="answer-container">{answers}</div>
       <div className="line"></div>
     </div>
   );
