e4e19d4af419174ec211af609aa8f9723721a174
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-12 21:03:27 +0100

    rewrite component, pass props and fix holding answer
    rewrite component because im stuck with pass logic

--- quizzical-app/src/components/Question.jsx
+++ quizzical-app/src/components/Question.jsx
@@ -1,51 +1,26 @@
-import { useState } from "react";
 import Answer from "./Answer";
-import { nanoid } from "nanoid";
 
 export default function Question(props) {
-  // tu sie odswieza ale czemu? Rekoncylacja???
+  function runHold(id) {
+    props.runHold(id, props.id);
+  }
+
   const answerElements = props.answers.map((answer) => {
-    const id = nanoid()
     return (
       <Answer
-        id={id}
-        key={id}
-        answer={answer}
-        clicked={false}
-        updateAnswers={updateAnswers}
+        id={answer.id}
+        key={answer.id}
+        questionId={props.id}
+        answer={answer.answer}
+        isHeld={answer.isHeld}
+        correct={answer.correct}
+        heldCorrect={answer.heldCorrect}
+        heldIncorrect={answer.heldIncorrect}
+        checked={answer.checked}
+        runHold={() => runHold(answer.id)}
       />
     );
   });
-
-  const [answers, setAnswers] = useState(answerElements);
-
-  function updateAnswers(id) {
-    setAnswers((oldAnswers) =>
-      oldAnswers.map((answer) => {
-        if (answer.props.id === id) {
-          return (
-            <Answer
-              id={answer.props.id}
-              key={answer.props.id}
-              answer={answer.props.answer}
-              clicked={true}
-              updateAnswers={updateAnswers}
-            />
-          );
-        }
-
-        return (
-          <Answer
-            id={answer.props.id}
-            key={answer.props.id}
-            answer={answer.props.answer}
-            clicked={false}
-            updateAnswers={updateAnswers}
-          />
-        );
-      })
-    );
-  }
 
   return (
     <div className="question-container">
@@ -55,7 +30,7 @@ export default function Question(props) {
         className="question-text"
         dangerouslySetInnerHTML={{ __html: props.question }}
       ></h3>
-      <div className="answer-container">{answers}</div>
+      <div className="answer-container">{answerElements}</div>
       <div className="line"></div>
     </div>
   );
