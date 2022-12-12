62c744c86527d110cf1ed43a67b141e485deeb0d
Kamil Pietrzyk <kam.pietrz@gmail.com>
2022-12-12 15:24:39 +0100

    add update logic for clicked answers and style

--- quizzical-app/src/components/Answer.jsx
+++ quizzical-app/src/components/Answer.jsx
@@ -1,7 +1,20 @@
 export default function Answer(props) {
+  let style = {};
+
+  if (props.clicked) {
+    style = {
+      border: "1px solid #4d5b9e",
+      backgroundColor: "#d6dbf5"
+    };
+  }
+
   return (
     <button
       className="answer-button"
+      style={style}
+      onClick={() => {
+        props.updateAnswers(props.id);
+      }}
       dangerouslySetInnerHTML={{ __html: props.answer }}
     ></button>
   );
